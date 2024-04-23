b3d35afe4b61ac1d7d2ca3f5f4c1061b824b594d
theSetox <stephen.siapno7@gmail.com>
2024-04-23 08:42:17 +0800

    Update MainViewModelTest
    Update and add new unit test for MainViewModelTest.
    Add MainDispatcherRule for MainViewModelTest.
    Bump the KSP plugin version to 1.9.23-1.0.20.
    
    Test: Can assemble project
    Test: Can run and install project and see the UI
    Test: SpotlessApply return no error
    Test: Smoke test for MainViewModelTest passed

--- app/src/test/java/com/thesetox/car/MainViewModelTest.kt
+++ app/src/test/java/com/thesetox/car/MainViewModelTest.kt
@@ -1,136 +1,313 @@
 package com.thesetox.car
 
-// TODO Update unit tests
-class MainViewModelTest
-// {
-//    @Test
-//    fun `when listOfCar is called, confirm if listOfCar in repository is called`() = runTest {
-//        // Arrange
-//        val repository =
-//            mock<CarRepository> {
-//                onBlocking { listOfCar() } doReturn Car.listOfCar()
-//            }
-//        val viewModel = MainViewModel(repository)
-//
-//        // Act
-//        viewModel.listOfCarState
-//
-//        // Assert
-//        verify(repository).listOfCar
-//    }
-//
-//    @Test
-//    fun `when listOfCar is called, confirm if empty listOfCar in repository is called`() {
-//        // Arrange
-//        val repository =
-//            mock<CarRepository> {
-//                on { listOfCar } doReturn emptyList()
-//            }
-//        val viewModel = MainViewModel(repository)
-//
-//        // Act
-//        viewModel.listOfCar
-//
-//        // Assert
-//        verify(repository).listOfCar
-//    }
-//
-//    @Test
-//    fun `when listOfMake is called, confirm if listOfMake in repository is called`() {
-//        // Arrange
-//        val repository =
-//            mock<CarRepository> {
-//                on { listOfMake } doReturn Car.listOfString()
-//            }
-//        val viewModel = MainViewModel(repository)
-//
-//        // Act
-//        viewModel.listOfMake
-//
-//        // Assert
-//        verify(repository).listOfMake
-//    }
-//
-//    @Test
-//    fun `when listOfMake is called, confirm if empty listOfMake in repository is called`() {
-//        // Arrange
-//        val repository =
-//            mock<CarRepository> {
-//                on { listOfMake } doReturn emptyList()
-//            }
-//        val viewModel = MainViewModel(repository)
-//
-//        // Act
-//        viewModel.listOfMake
-//
-//        // Assert
-//        verify(repository).listOfMake
-//    }
-//
-//    @Test
-//    fun `when listOfModel is called, confirm if listOfModel in repository is called`() {
-//        // Arrange
-//        val repository =
-//            mock<CarRepository> {
-//                on { listOfModel } doReturn Car.listOfString()
-//            }
-//        val viewModel = MainViewModel(repository)
-//
-//        // Act
-//        viewModel.listOfModel
-//
-//        // Assert
-//        verify(repository).listOfModel
-//    }
-//
-//    @Test
-//    fun `when listOfModel is called, confirm if empty listOfModel in repository is called`() {
-//        // Arrange
-//        val repository =
-//            mock<CarRepository> {
-//                on { listOfModel } doReturn emptyList()
-//            }
-//        val viewModel = MainViewModel(repository)
-//
-//        // Act
-//        viewModel.listOfModel
-//
-//        // Assert
-//        verify(repository).listOfModel
-//    }
-//
-//    @Test
-//    fun `when filter is needed, confirm if filterListOfCar in repository is called`() {
-//        // Arrange
-//        val repository =
-//            mock<CarRepository> {
-//                on { filterListOfCar("", "") } doReturn Car.listOfCar()
-//            }
-//        val viewModel = MainViewModel(repository)
-//
-//        // Act
-//        val applyFilter = viewModel.onSelectedFilter()
-//        applyFilter("", "")
-//
-//        // Assert
-//        verify(repository).filterListOfCar("", "")
-//    }
-//
-//    @Test
-//    fun `when filter is needed, confirm if empty filterListOfCar in repository is called`() {
-//        // Arrange
-//        val repository =
-//            mock<CarRepository> {
-//                on { listOfCar } doReturn Car.listOfCar()
-//                on { filterListOfCar("", "") } doReturn emptyList()
-//            }
-//        val viewModel = MainViewModel(repository)
-//
-//        // Act
-//        val applyFilter = viewModel.onSelectedFilter()
-//        applyFilter("", "")
-//
-//        // Assert
-//        verify(repository).listOfCar
-//    }
-// }
+import com.thesetox.car.data.repository.CarRepository
+import com.thesetox.car.model.Car
+import com.thesetox.car.model.Car.Companion.listOfCar
+import com.thesetox.car.model.Car.Companion.listOfString
+import com.thesetox.car.viewmodel.Action
+import com.thesetox.car.viewmodel.MainViewModel
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+
+class MainViewModelTest {
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+
+    @Test
+    fun `when listOfCar is called, confirm if listOfCar in repository is called`() =
+        runTest {
+            // Arrange
+            val repository =
+                mock<CarRepository> {
+                    onBlocking { listOfCar() } doReturn Car.listOfCar()
+                }
+            val viewModel = MainViewModel(repository)
+
+            // Act
+            viewModel.onAction(Action.LoadCarList)
+
+            // Assert
+            verify(repository).listOfCar()
+        }
+
+    @Test
+    fun `when listOfCar is called, confirm if empty listOfCar in repository is called`() =
+        runTest {
+            // Arrange
+            val repository =
+                mock<CarRepository> {
+                    onBlocking { listOfCar() } doReturn emptyList()
+                }
+            val viewModel = MainViewModel(repository)
+
+            // Act
+            viewModel.onAction(Action.LoadCarList)
+
+            // Assert
+            verify(repository).listOfCar()
+        }
+
+    @Test
+    fun `when listOfCar is called, confirm if listOfCarState can get the result`() =
+        runTest {
+            // Arrange
+            val repository =
+                mock<CarRepository> {
+                    onBlocking { listOfCar() } doReturn Car.listOfCar()
+                }
+            val viewModel = MainViewModel(repository)
+
+            // Act
+            viewModel.onAction(Action.LoadCarList)
+            val result = viewModel.listOfCarState.value
+
+            // Assert
+            val expectedResult = Car.listOfCar()
+            Assert.assertEquals(expectedResult, result)
+        }
+
+    @Test
+    fun `when listOfCar is called, confirm if listOfCarState can get the empty result`() =
+        runTest {
+            // Arrange
+            val repository =
+                mock<CarRepository> {
+                    onBlocking { listOfCar() } doReturn emptyList()
+                }
+            val viewModel = MainViewModel(repository)
+
+            // Act
+            viewModel.onAction(Action.LoadCarList)
+            val result = viewModel.listOfCarState.value
+
+            // Assert
+            val expectedResult = emptyList<Car>()
+            Assert.assertEquals(expectedResult, result)
+        }
+
+    @Test
+    fun `when listOfMake is called, confirm if listOfMake in repository is called`() =
+        runTest {
+            // Arrange
+            val repository =
+                mock<CarRepository> {
+                    onBlocking { listOfMake() } doReturn Car.listOfString()
+                }
+            val viewModel = MainViewModel(repository)
+
+            // Act
+            viewModel.onAction(Action.LoadMakeList)
+
+            // Assert
+            verify(repository).listOfMake()
+        }
+
+    @Test
+    fun `when listOfMake is called, confirm if empty listOfMake in repository is called`() {
+        runTest {
+            // Arrange
+            val repository =
+                mock<CarRepository> {
+                    onBlocking { listOfMake() } doReturn emptyList()
+                }
+            val viewModel = MainViewModel(repository)
+
+            // Act
+            viewModel.onAction(Action.LoadMakeList)
+
+            // Assert
+            verify(repository).listOfMake()
+        }
+    }
+
+    @Test
+    fun `when listOfMake is called, confirm if listOfMakeState can get the result`() =
+        runTest {
+            // Arrange
+            val repository =
+                mock<CarRepository> {
+                    onBlocking { listOfMake() } doReturn Car.listOfString()
+                }
+            val viewModel = MainViewModel(repository)
+
+            // Act
+            viewModel.onAction(Action.LoadMakeList)
+            val result = viewModel.listOfMakeState.value
+
+            // Assert
+            val expectedResult = Car.listOfString()
+            Assert.assertEquals(expectedResult, result)
+        }
+
+    @Test
+    fun `when listOfMake is called, confirm if listOfMakeState can get the empty result`() {
+        runTest {
+            // Arrange
+            val repository =
+                mock<CarRepository> {
+                    onBlocking { listOfMake() } doReturn emptyList()
+                }
+            val viewModel = MainViewModel(repository)
+
+            // Act
+            viewModel.onAction(Action.LoadMakeList)
+            val result = viewModel.listOfMakeState.value
+
+            // Assert
+            val expectedResult = emptyList<String>()
+            Assert.assertEquals(expectedResult, result)
+        }
+    }
+
+    @Test
+    fun `when listOfModel is called, confirm if listOfModel in repository is called`() =
+        runTest {
+            // Arrange
+            val repository =
+                mock<CarRepository> {
+                    onBlocking { listOfModel() } doReturn Car.listOfString()
+                }
+            val viewModel = MainViewModel(repository)
+
+            // Act
+            viewModel.onAction(Action.LoadModelList)
+
+            // Assert
+            verify(repository).listOfModel()
+        }
+
+    @Test
+    fun `when listOfModel is called, confirm if empty listOfModel in repository is called`() {
+        runTest {
+            // Arrange
+            val repository =
+                mock<CarRepository> {
+                    onBlocking { listOfModel() } doReturn emptyList()
+                }
+            val viewModel = MainViewModel(repository)
+
+            // Act
+            viewModel.onAction(Action.LoadModelList)
+
+            // Assert
+            verify(repository).listOfModel()
+        }
+    }
+
+    @Test
+    fun `when listOfModel is called, confirm if listOfModelState can get the result`() =
+        runTest {
+            // Arrange
+            val repository =
+                mock<CarRepository> {
+                    onBlocking { listOfModel() } doReturn Car.listOfString()
+                }
+            val viewModel = MainViewModel(repository)
+
+            // Act
+            viewModel.onAction(Action.LoadModelList)
+            val result = viewModel.listOfModelState.value
+
+            // Assert
+            val expectedResult = Car.listOfString()
+            Assert.assertEquals(expectedResult, result)
+        }
+
+    @Test
+    fun `when listOfModel is called, confirm if listOfMakeState can get the empty result`() {
+        runTest {
+            // Arrange
+            val repository =
+                mock<CarRepository> {
+                    onBlocking { listOfModel() } doReturn emptyList()
+                }
+            val viewModel = MainViewModel(repository)
+
+            // Act
+            viewModel.onAction(Action.LoadModelList)
+            val result = viewModel.listOfModelState.value
+
+            // Assert
+            val expectedResult = emptyList<String>()
+            Assert.assertEquals(expectedResult, result)
+        }
+    }
+
+    @Test
+    fun `when filter is needed, confirm if filterListOfCar in repository is called`() =
+        runTest {
+            // Arrange
+            val repository =
+                mock<CarRepository> {
+                    onBlocking { filterListOfCar("", "") } doReturn Car.listOfCar()
+                }
+            val viewModel = MainViewModel(repository)
+
+            // Act
+            viewModel.onAction(Action.FilterList("", ""))
+
+            // Assert
+            verify(repository).filterListOfCar("", "")
+        }
+
+    @Test
+    fun `when filter is needed, confirm if empty listOfMakeState in repository is called`() {
+        runTest {
+            // Arrange
+            val repository =
+                mock<CarRepository> {
+                    onBlocking { filterListOfCar("", "") } doReturn emptyList()
+                }
+            val viewModel = MainViewModel(repository)
+
+            // Act
+            viewModel.onAction(Action.FilterList("", ""))
+
+            // Assert
+            verify(repository).filterListOfCar("", "")
+        }
+    }
+
+    @Test
+    fun `when filter is needed, confirm if listOfCarState can get the empty result`() =
+        runTest {
+            // Arrange
+            val repository =
+                mock<CarRepository> {
+                    onBlocking { filterListOfCar("", "") } doReturn emptyList()
+                }
+            val viewModel = MainViewModel(repository)
+
+            // Act
+            viewModel.onAction(Action.FilterList("", ""))
+            val result = viewModel.listOfCarState.value
+
+            // Assert
+            val expectedResult = emptyList<Car>()
+            Assert.assertEquals(expectedResult, result)
+        }
+
+    @Test
+    fun `when filter is needed, confirm if listOfCarState can get the result`() =
+        runTest {
+            // Arrange
+            val repository =
+                mock<CarRepository> {
+                    onBlocking { filterListOfCar("", "") } doReturn Car.listOfCar()
+                }
+            val viewModel = MainViewModel(repository)
+
+            // Act
+            viewModel.onAction(Action.FilterList("", ""))
+            val result = viewModel.listOfCarState.value
+
+            // Assert
+            val expectedResult = Car.listOfCar()
+            Assert.assertEquals(expectedResult, result)
+        }
+}
